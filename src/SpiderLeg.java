bd6fe72504034ef1d75ea8d622f2d32839a9e0f1
Alexandru Preda <93654677+alexpreda24@users.noreply.github.com>
2022-12-24 15:31:03 +0200

    Update SpiderLeg.java

--- src/SpiderLeg.java
+++ src/SpiderLeg.java
@@ -15,8 +15,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class SpiderLeg
-{
+public class SpiderLeg {
     // We'll use a fake USER_AGENT so the web server thinks the robot is a normal web browser.
     private static final String USER_AGENT =
             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
@@ -28,14 +27,11 @@ public class SpiderLeg
      * This performs all the work. It makes an HTTP request, checks the response, and then gathers
      * up all the links on the page. Perform a searchForWord after the successful crawl
      *
-     * @param url
-     *            - The URL to visit
+     * @param url - The URL to visit
      * @return whether or not the crawl was successful
      */
-    public boolean crawl(String url)
-    {
-        try
-        {
+    public boolean crawl(String url) {
+        try {
             Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
             Document htmlDocument = connection.get();
             this.htmlDocument = htmlDocument;
@@ -44,40 +40,36 @@ public class SpiderLeg
 //            if(linksOnPage == null) linksOnPage = htmlDocument.getElementById("contact-us");
 
             return true;
-        }
-        catch(IOException ioe)
-        {
+        } catch (IOException ioe) {
             // We were not successful in our HTTP request
             return false;
         }
     }
+
     /**
      * Performs a search on the body of on the HTML document that is retrieved. This method should
      * only be called after a successful crawl.
      *
-     * @param searchWord
-     *            - The word or string to look for
+     * @param searchWord - The word or string to look for
      * @return whether or not the word was found
      */
-    public void searchForWord(String searchWord, Set<String> emails,Set<String> phone)
-    {
-        try{
+    public void searchForWord(String searchWord, Set<String> emails, Set<String> phone) {
+        try {
             URL url = new URL(searchWord);
-            if(!crawl(searchWord))
+            if (!crawl(searchWord))
                 throw new MalformedURLException("ERROR! Call crawl() before performing analysis on the document");
-            if(this.htmlDocument == null)
-            {
+            if (this.htmlDocument == null) {
                 throw new MalformedURLException("ERROR! Call crawl() before performing analysis on the document");
             }
             String input = this.htmlDocument.toString();
             try {
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
 
 
 //                String pageSource = IOUtils.toString(connection.getInputStream(), "UTF-8");
-            }catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             Elements emailLinks = htmlDocument.select("a[href^=mailto:]");
@@ -93,18 +85,18 @@ public class SpiderLeg
             Pattern pattern1 = Pattern.compile("(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]\\d{3}[\\s.-]\\d{4}", Pattern.CASE_INSENSITIVE);
             Matcher matchs1 = pattern1.matcher(input);
             while (matchs.find()) {
-                    emails.add(matchs.group());
+                emails.add(matchs.group());
             }
             while (matchs1.find()) {
                 phone.add(matchs1.group());
             }
-        }catch(MalformedURLException e){
+        } catch (MalformedURLException e) {
             System.out.println("ERROR! Call crawl() before performing analysis on the document");
             e.printStackTrace();
         }
     }
-    public List<String> getLinks()
-    {
+
+    public List<String> getLinks() {
         return this.links;
     }
 
